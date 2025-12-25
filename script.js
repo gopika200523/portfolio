4359dbea232e8d728b50df7c18a2d1a47aed9f82
gopika200523 <gopika200523@gmail.com>
2025-12-25 10:14:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Portfolio slider
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+  portfolioDetails.forEach((detail) => detail.classList.remove("activess"));
+  portfolioDetails[index].classList.add("activess");
+};
+
+// Right button
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+  activePortfolio();
+});
+
+// Left button
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+  activePortfolio();
+});
